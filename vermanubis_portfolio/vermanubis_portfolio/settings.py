18d35f55a06b1c240504e76e07ed28641a3521d6
Jason Shirley <jshirley@eou.edu>
2020-05-21 16:36:12 -0600

    changed settings

--- vermanubis_portfolio/vermanubis_portfolio/settings.py
+++ vermanubis_portfolio/vermanubis_portfolio/settings.py
@@ -26,7 +26,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'dp2*b!-)5$9_=cu(q@+3=up+xl_34&2crp0&$bf^dn_j$tg3@1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '[::1]' 'vermanubisportfolio.herokuapp.com']
 
